c616e2909ead6d7d77d183ac54cdd1122c28d9dc
Brett van Zuiden <brett.vanzuiden@clever.com>
2017-06-15 20:52:13 -0700

    A little more fantsy

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Coming soon!')
+app.get('/', function(req, res) {
+  res.render('index', {});
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 })
