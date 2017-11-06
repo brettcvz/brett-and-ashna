0d2ffe31556a9ec05758460464a49b5b76a150d1
Brett van Zuiden <brett.vanzuiden@clever.com>
2017-11-05 22:40:08 -0800

    Added RSVP functionality

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var app = express();
 
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var auth = new googleAuth();
+var oauth2Client = new auth.OAuth2(process.env.GDOCS_CLIENT_ID, process.env.GDOCS_CLIENT_SECRET, "http://brettandashna.com/api/authorize");
+oauth2Client.credentials = JSON.parse(process.env.GDOCS_CREDENTIALS);
+var sheets = google.sheets('v4');
+
+var RSVP_SHEET = process.env.GDOCS_RSVP_SHEET;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+
+var RSVP_DATA = [];
+function loadRSVPData(cb) {
+  sheets.spreadsheets.values.get({
+    auth: oauth2Client,
+    spreadsheetId: RSVP_SHEET,
+    range: 'RSVPs!A2:G'
+  }, function(err, response) {
+    if (err) {
+      return cb(err);
+    }
+
+    var rows = response.values;
+    var data = rows.map(function(row, i) {
+      return {
+        name: row[0],
+        code: row[1],
+        additionalGuests: parseInt(row[2], 10),
+        row: i
+      };
+    });
+    cb(undefined, data);
+  });
+}
+
+loadRSVPData(function(err, data){
+  if (err) {
+    console.error(err);
+    // Fail hard on boot
+    throw err;
+  }
+
+  RSVP_DATA = data;
+});
 
 app.get('/', function(req, res) {
   res.render('staging', {});
@@ -11,6 +59,87 @@ app.get('/', function(req, res) {
 
 app.get('/preview', function(req, res) {
   res.render('index', {});
+});
+
+app.get('/api/refresh', function(req, res) {
+  loadRSVPData(function(err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500);
+      return res.send(err);
+    }
+
+    RSVP_DATA = data;
+    return res.send("Success!");
+  });
+});
+
+app.get('/api/guests', function(req, res) {
+  var search = req.query.search;
+  var items = [];
+  if (search && search.length && search.length >= 5) {
+    var lowerCase = search.toLowerCase();
+    items = RSVP_DATA.filter(function(row) {
+      return row.name.toLowerCase().indexOf(lowerCase) >= 0;
+    });
+  }
+
+  return res.json({
+    items: items,
+    search: search
+  });
+});
+
+app.post('/api/rsvp', function(req, res) {
+  console.log(req.body);
+  if (!req.body) { return res.status(400); }
+
+  var rsvp = req.body.rsvp;
+  var guestNames = req.body.guestNames;
+  var groupTrip = req.body.groupTrip;
+  var notes = req.body.notes;
+  var code = req.body.code;
+  var row = parseInt(req.body.row, 10) + 2; // Accounting for header, plus 0 -> 1 offset
+
+  if (!rsvp || !row || !code) {
+    return res.status(400).send("Missing data");
+  }
+
+  sheets.spreadsheets.values.get({
+    auth: oauth2Client,
+    spreadsheetId: RSVP_SHEET,
+    range: 'RSVPs!A'+row+':G'+row
+  }, function(err, response) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error connecting to google");
+    }
+
+    var rows = response.values;
+    if (rows.length === 0) {
+      return res.status(400).send("Invalid row");
+    }
+    var trueCode = rows[0][1];
+    if (code !== trueCode) {
+      return res.status(400).send("Invalid code");
+    }
+
+    var entry = [ rsvp, guestNames, groupTrip, notes ];
+    sheets.spreadsheets.values.update({
+      auth: oauth2Client,
+      spreadsheetId: RSVP_SHEET,
+      range: 'RSVPs!D'+row+':G'+row,
+      valueInputOption: "RAW",
+      resource: { values: [ entry ] }
+    }, function(err, response) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error connecting to google");
+      }
+
+      res.send("Success");
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,90 @@ $(function(){
     dots: true,
   });
   $.modal.defaults.fadeDuration = 200;
+
+  $("#rsvp-form .button-group button").click(function(){
+    $(this).siblings().removeClass("selected");
+    $(this).addClass("selected");
+  });
+
+  $("#rsvp-form .yes-no button").click(function(){
+    $("#rsvp-form #submit").removeClass("disabled");
+    var rsvp = $("#rsvp-form .yes-no .selected").attr("name");
+    if (rsvp === "yes") {
+      $("#rsvp-form .additional-info").show();
+    } else {
+      $("#rsvp-form .additional-info").hide();
+    }
+  });
+
+  var nameLookup = $("#lookup-name");
+  var selectedPerson;
+  nameLookup.easyAutocomplete({
+    url: function(phrase) {
+      if (phrase.length < 5) {
+        return;
+      }
+      return "/api/guests?search="+phrase;
+    },
+    placeholder: "Please enter your name to look up your reservation",
+    getValue: "name",
+    listLocation: "items",
+    matchResponseProperty: "search",
+    list: {
+      match: { enabled: true },
+      maxNumberOfElements: 3,
+      onChooseEvent: function(){
+        selectedPerson = nameLookup.getSelectedItemData();
+        var nameString = selectedPerson.name;
+        if (selectedPerson.additionalGuests === 1) {
+          nameString += " and Guest";
+        } else if (selectedPerson.additionalGuests > 1) {
+          nameString += " and Guests";
+        }
+
+        if (selectedPerson.additionalGuests >= 1) {
+          $("#rsvp-form .yes-no button[name='yes']").text("Yes, we will attend");
+          $("#rsvp-form .yes-no button[name='no']").text("No, we can't make it");
+        }
+        $("#rsvp-form .name").text(nameString);
+        var partyNames = $("#rsvp-form .party-names");
+        for (var i=0; i < selectedPerson.additionalGuests; i++) {
+          var placeholder = (selectedPerson.additionalGuests === 1) ?
+            "Name of guest" : "Name of guest "+(i+1);
+          partyNames.append("<input placeholder='"+placeholder+"'/>");
+        }
+
+        $("#lookup-form").hide();
+        $("#rsvp-form").show();
+      }
+    }
+  });
+
+  $("#rsvp-form #submit").click(function(){
+    if ($(this).hasClass("disabled")) {
+      return;
+    }
+    var rsvp = $("#rsvp-form .yes-no .selected").attr("name");
+    var guestNames = "";
+    if (selectedPerson.additionalGuests > 0) {
+      guestNames = $("#rsvp-form .party-names input").get().map(function(guest){
+        return guest.value;
+      }).join(", ");
+    }
+    var groupTrip = $("#rsvp-form .travel-plans button.selected").attr("name");
+    var notes = $("#rsvp-form #notes").val();
+
+    $.post("/api/rsvp", {
+      rsvp: rsvp,
+      guestNames: rsvp === "yes" ? guestNames : "",
+      groupTrip: rsvp === "yes" ? groupTrip : "",
+      notes: notes,
+      row: selectedPerson.row,
+      code: selectedPerson.code,
+    });
+
+    $("#rsvp-form").hide();
+    $("#thanks").show();
+    $("#thanks p."+rsvp).show();
+  });
 });
